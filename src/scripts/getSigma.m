7adfe6a6284cc73014346d4b5be90cc5f9b27506
iostream6 <oilamah@gmail.com>
2018-07-15 14:48:05 +0100

    Added implementation of heuristic to compute sigma for GRBF

--- src/scripts/getSigma.m
+++ src/scripts/getSigma.m
@@ -1,28 +1,34 @@
 
 function sigma = getSigma(Xrow, param)
-% GETSIGMA  estimates sigma for the GRBF kernel based on heuristics   
-% This code is based on MATLAB implementations provided by Hai Vo
-% 
-%   Syntax: sigma = getSigma(Xrow, param)
-%
-%   Inputs: 
-%       Xrow  - The complete training samples/realizations, row based
-%      param  - user supplied input parameter used in the estimation of sigma
-%
-%   Outputs: 
-%        sigma    - the estimated sigma for the input dataset
+   % GETSIGMA  estimates sigma for the GRBF kernel based on heuristics   
+   % This code is based on MATLAB implementations provided by Hai Vo
+   % 
+   %   Syntax: sigma = getSigma(Xrow, param)
+   %
+   %   Inputs: 
+   %       Xrow  - The complete training samples/realizations, row based
+   %      param  - user supplied input parameter used in the estimation of sigma
+   %
+   %   Outputs: 
+   %        sigma    - the estimated sigma for the input dataset
+   
+   %   Examples: <provide examples here>
+   %
+   %   Notes:  <provide notes here>
+   %
+   %   References:  
+   %      "New Geological Parameterizations for History Matching Complex Models". Hai Vo. Stanford University PhD thesis. 2015.
+   %      "Kernel Methods in Computer Vision". Christoph H. Lampert. http://mr.crossref.org/iPage?doi=10.1561%2F0600000027
+   %      "Multiobjective history matching with dominance and decomposition". Osho Ilamah. Journal paper (in press).
+   %
+   %   See also: <upper case comma separated list of related files and functions here>
+   %
+   %   $Author: Ilamah, Osho $ $Date:2018.07.15 $ $Revision: 0.1  
 
-%   Examples: <provide examples here>
-%
-%   Notes:  <provide notes here>
-%
-%   References:  
-%      "New Geological Parameterizations for History Matching Complex Models". Hai Vo. Stanford University PhD thesis. 2015.
-%      "The pre-image problem in kernel methods". James Kwok & Ivor Tsang. ICML-2003 conference paper. 2003.
-%      "Multiobjective history matching with dominance and decomposition". Osho Ilamah. Journal paper (in press).
-%
-%   See also: <upper case comma separated list of related files and functions here>
-%
-%   $Author: Ilamah, Osho $ $Date:2018.07.13 $ $Revision: 0.1  
+   squaredDistances = sdm(Xrow);
+   % unrolling into a vector
+   squaredDistances = squaredDistances(:);
+   % Modified from Lampert, following Vo
+   sigma = param * sqrt(median(squaredDistances) / 2);
 
 end
